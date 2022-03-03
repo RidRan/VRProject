ad3916a08f28d02c6cf86f54cf5a66d272003c8b
Michael Rahe <42557390+MRahe@users.noreply.github.com>
2022-03-03 17:01:16 -0600

    added saber

--- Assets/Scripts/ToppingCollisionManager.cs
+++ Assets/Scripts/ToppingCollisionManager.cs
@@ -7,6 +7,8 @@ public class ToppingCollisionManager : MonoBehaviour
 {
     private Rigidbody rb;
 
+    public Hand hand;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class ToppingCollisionManager : MonoBehaviour
     
     public void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.CompareTag("Base"))
+        if((other.gameObject.CompareTag("Topping")) && (SushiManager.instance.holding == null))
         {
             Destroy(other.gameObject.GetComponent<Throwable>());
             Destroy(other.gameObject.GetComponent<Rigidbody>());
