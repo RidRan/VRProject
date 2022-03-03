203473c5019c63c404cefe3239b5aefd181cb379
Riddock Moran <rmoran3@unl.edu>
2022-03-03 16:59:32 -0600

    Added sample boss

--- Assets/Scripts/GenerateBuidlings.cs
+++ Assets/Scripts/GenerateBuidlings.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GenerateBuidlings : MonoBehaviour
 {
-    public GameObject starter;
+    public GameObject starterBuilding;
 
     public float startDistance;
     public float endDistance;
@@ -15,12 +15,7 @@ public class GenerateBuidlings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float distance = startDistance;
-        while (distance < endDistance)
-        {
-            distance += GenerateBuilding(distance);
-        }
-        Destroy(starter);
+        GenerateCitySection();
     }
 
     // Update is called once per frame
@@ -29,11 +24,21 @@ public class GenerateBuidlings : MonoBehaviour
         
     }
 
+    public void GenerateCitySection()
+    {
+        float distance = startDistance;
+        while (distance < endDistance)
+        {
+            distance += GenerateBuilding(distance);
+        }
+        Destroy(starterBuilding);
+    }
+
     private float GenerateBuilding(float distance)
     {
         Vector3 scale = new Vector3(Random.Range(avg.x - std.x, avg.x + std.x), Random.Range(avg.y - std.y, avg.y + std.y), Random.Range(avg.z - std.z, avg.z + std.z));
         
-        GameObject newBuilding = Instantiate(starter, new Vector3(distance + scale.x / 2, scale.y / 2, transform.position.z), starter.transform.localRotation, transform);
+        GameObject newBuilding = Instantiate(starterBuilding, new Vector3(distance + scale.x / 2 + transform.position.x, scale.y / 2, transform.position.z), starterBuilding.transform.localRotation, transform);
         newBuilding.transform.localScale = scale;
 
         return scale.x;
--- Assets/Scripts/GenerateCity.cs
+++ Assets/Scripts/GenerateCity.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GenerateCity : MonoBehaviour
 {
+    public GameObject starterSection;
+
+    public int numSections;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateCitySections();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GenerateCitySections()
+    {
+        for(int i = -numSections; i < numSections; i++)
+        {
+            Debug.Log("Generated city section: " + (i + numSections + 1));
+            GameObject newSection = Instantiate(starterSection, new Vector3(transform.position.x + 30 * i, transform.position.y, transform.position.z), starterSection.transform.localRotation, transform);
+        }
+        Destroy(starterSection);
     }
 }
