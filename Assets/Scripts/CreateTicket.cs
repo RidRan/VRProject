22bf0f241aab5478cc663bfbddfa2b1d07262854
Michael Rahe <42557390+MRahe@users.noreply.github.com>
2022-04-07 17:01:12 -0500

    Work on new knife and ticket spawning

--- Assets/Scripts/CreateTicket.cs
+++ Assets/Scripts/CreateTicket.cs
@@ -1,33 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Order
+{
+    public Image picture;
+    public int amount;
+}
 
 public class CreateTicket : MonoBehaviour
 {
-    /*
-    public Dictionary < int, Image > setFoPics = new Dictionary < string, Image > ();
-
-    public class Order
-    {
-        public Image picture;
-        public int amount;
-        
-        public createOrder(Image pic, int amt)
-        {
-            picture = pic;
-            amount = amt;
-        }
+    [SerializeField] private List<Order> orders;
+    
+    public Order CreateATicket() { 
+        return orders[UnityEngine.Random.Range(0, orders.Count)];
     }
-
-    public void createATicket()
-    {
-        int setNumber = Random.Range(0, sizeof(setFoPics));
-        int qnt = Random.Range(1, 3);
-
-       Order newOrder =  createOrder(setFoPics[setNumber], qnt);
-
-        //return new GameObject joe;
-
-    }
-    */
 }
