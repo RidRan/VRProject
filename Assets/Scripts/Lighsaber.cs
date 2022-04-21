05af263ba50ea059f987a41c1fbbc81086c70bd6
Riddock Moran <45524258+driedel152@users.noreply.github.com>
2022-04-21 17:33:39 -0500

    cart changes

--- Assets/Scripts/Lighsaber.cs
+++ Assets/Scripts/Lighsaber.cs
@@ -79,6 +79,7 @@ public class Lighsaber : MonoBehaviour
         }
 
         //Draw first triangle vertices for back and front
+        Debug.Log("_base.transform.position " + _base.transform.position);
         _vertices[_frameCount] = _base.transform.position;
         _vertices[_frameCount + 1] = _tip.transform.position;
         _vertices[_frameCount + 2] = _previousTipPosition;
@@ -159,6 +160,7 @@ public class Lighsaber : MonoBehaviour
 
         if(sliceable != null)
         {
+            Debug.Log("fuck");
             GameObject[] slices = Slicer.instance.Slice(plane, other.gameObject);
             Destroy(other.gameObject);
 
