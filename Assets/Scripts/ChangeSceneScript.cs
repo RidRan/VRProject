5f9821e3768f47ffdba5f4d7ffe31752ed321115
Riddock Moran <rmoran3@unl.edu>
2022-04-28 16:08:16 -0500

    cutscene automatically changes

--- Assets/Scripts/ChangeSceneScript.cs
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -8,6 +8,20 @@ namespace Valve.VR.InteractionSystem.Sample
     public class ChangeSceneScript : MonoBehaviour
     {
         public string loadLevelName;
+        public bool cutscene = false;
+        private int cutsceneLength = 600;
+        private int counter = 0;
+
+        void FixedUpdate()
+        {
+
+            counter++;
+            if (cutscene && counter > cutsceneLength)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(loadLevelName);
+            }
+        }
+
 
         public void OnButtonDown(Hand fromHand)
         {
